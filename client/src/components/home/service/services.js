687f8e2efc7549b755b9e21c50ef9ed8056bb42f
nouha hammami <nouhahammami21@gmail.com>
2023-04-30 14:14:38 +0200

    addHome

---
+++ client/src/components/home/service/services.js
@@ -0,0 +1,230 @@
+import React from "react";
+
+function Service ()  {
+    return (
+        <div className="service component__space" id="Services">
+            <div className="headingg">
+                <br />
+                <h1 className="headingg">Our Services</h1>
+                <br />
+               
+                
+            </div>
+
+            <div className="container">
+                <div className="row">
+
+                <div id="mes-elements" className="grid-container">
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 16.1A5 5 0 0 1 5.9 20M2 12.05A9 9 0 0 1 9.95 20M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6"></path>
+                  <line x1="2" y1="20" x2="2" y2="20"></line>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text">Recruitment and Staffing Solutions</h1>
+                                <p className="p service__text p__color">
+                                We provide end-to-end recruitment and staffing solutions to help you find the right talent for your organization. 
+                                </p>
+                                <p className="p service__text p__color">
+                                Our services include sourcing, screening, and onboarding candidates, as well as managing payroll and benefits.
+                                </p>
+                                
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                  <polyline points="2 17 12 22 22 17"></polyline>
+                  <polyline points="2 12 12 17 22 12"></polyline>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text">Job Placement Services</h1>
+                                <p className="p service__text p__color">
+                                Our job placement services are designed to help job seekers find their dream job while providing employers with the best possible candidates. 
+                                </p>
+                                <p className="p service__text p__color">
+                                We work with job seekers to understand their skills and career aspirations, matching them with opportunities that align with their goals.
+                                </p>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text"> CV Scanning and Analysis</h1>
+                                <p className="p service__text p__color">
+                                Our CV scanning and analysis service uses advanced algorithms to scan and analyze resumes, identifying key skills, experiences, and qualifications to match job requirements.
+                                </p>
+                                <p className="p service__text p__color">
+                                This helps to streamline the recruitment process, saving time and effort for both job seekers and employers.
+                                </p>
+                                <p className="p service__text p__color">
+                                Our technology is constantly updated to ensure the highest accuracy and efficiency in identifying the best candidates for your job openings.
+                                </p>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text">Candidate Evaluation</h1>
+                                <p className="p service__text p__color">
+                                Our quiz helps evaluate candidates by assessing their skills and qualifications. 
+                                </p>
+                                <p className="p service__text p__color">
+                                With customizable questions, you can quickly and efficiently determine their strengths and weaknesses, making better hiring decisions for your organization. 
+                                </p>
+                                <p className="p service__text p__color">
+                                Improve your recruitment process and build a stronger team with our Candidate Quiz Evaluation.
+                                </p>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text">Career Advancement Services</h1>
+                                <p className="p service__text p__color">
+                                Our website offers expert guidance and training to help you advance your career. From resume writing to job search strategies, we provide personalized advice to help you achieve your goals. 
+                                </p>
+                                <p className="p service__text p__color">
+                                Our training courses cover a range of in-demand skills and are designed to prepare you for today's job market.
+                                </p>
+                                <p className="p service__text p__color">
+                                Take the first step towards career success with our Career Advancement Services.
+                                </p>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div className="col__3">
+                         <div className="service__box pointer">
+                            <div className="iconn">
+                            <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+                            </div>
+                            <div className="service__meta">
+                                <h1 className="service__text">Transport Assistance</h1>
+                                <p className="p service__text p__color">
+                                Our website provides an easy way for candidates to find their interview location and transportation options. 
+                                </p>
+                                <p className="p service__text p__color">
+                                With our technology, candidates can quickly get directions to the interview site, including public transportation routes and travel time. This service helps reduce stress and increase efficiency for both candidates and hiring companies.
+                                </p>
+                                <p className="p service__text p__color">
+                                Simplify your hiring process with our Interview Location and Transport Services.
+                                </p>
+                            </div>
+                            
+                         </div>
+                    </div>
+                    <br />
+                <br />
+                </div>
+            </div>
+        </div>
+        </div>
+      
+      
+    )
+}
+
+export default Service;
+ 
