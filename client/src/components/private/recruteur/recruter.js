ab779b5e19d6ea1683f147735ade1a6fcb54536b
intissar.nasr <nasrintissar05@gmail.com>
2023-05-07 19:55:57 +0200

    affichage de liste d'offre postule

--- client/src/components/private/recruteur/recruter.js
+++ client/src/components/private/recruteur/recruter.js
@@ -1,8 +1,132 @@
-import React from 'react';
+import React, {useState, useEffect, useContext } from 'react';
 import Header from '../../layout/header';
 import SidebarRecruteur from '../../layout/sidebarRecruteur';
-function Recruter() {
+import { useNavigate  } from 'react-router-dom';
+import useFetch from '../../../hooks/fetch.hook';
+import convertToBase64 from '../../../helper/convert';
+import { updateUser } from '../../../helper/helper';
+import { useFormik } from 'formik';
+import jwt_decode from 'jwt-decode';
+
+   import { profileValidation } from '../../../helper/validate';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+function Recruter(props) {
+  const [getoffredata, setOffredata] = useState([]);
+  console.log(getoffredata);
+  const [getuserdata, setuserdata] = useState([]);
+  console.log(getuserdata);
+  const [search,setSearch]=useState('');
+  console.log(search);
+      const [file, setFile] = useState();
+      const [{ isLoading, apiData, serverError }] = useFetch();
+    
+    
+  
+        const [offres, setOffres] = useState([]);
+      
+        useEffect(() => {
+          const fetchData = async () => {
+            const result = await axios.get(`/o/${props.userId}`);
+            setOffres(result.data);
+          };
+          fetchData();
+        }, [props.userId]);
+      
+      
+ 
+        const getdata = async () => {
+
+      const token = localStorage.getItem('token')
+         if(!token) return Promise.reject("Cannot find Token");
+        let decode = jwt_decode(token)
+              const res = await fetch(`http://localhost:8080/api/o/${decode?.userId}`, {
+                  method: "GET",
+                 headers: {
+                                        "Content-Type": "application/json"
+                  }
+              });
+            const data = await res.json();
+              console.log(data);
+              if (res.status === 422 || !data) {
+                  console.log("error ");
+            } else {
+                setOffredata(data)
+                  console.log("get data");
+              }
+        };
+   console.log("offre=>",getoffredata)
+        useEffect(() => {
+            getdata();
+        }, [])
+    // const getdatauser = async () => {
+    // const token = localStorage.getItem('token')
+    // if(!token) return Promise.reject("Cannot find Token");
+  //     let decode = jwt_decode(token)
+  //          const res = await fetch(`http://localhost:8080/api/getalldata/${decode?.offreId}`, {
+  //              method: "GET",
+  //              headers: {
+  //                  "Content-Type": "application/json"
+  //              }
+  //          });
+  //          const data = await res.json();
+  //          console.log(data);
+  //          if (res.status === 422 || !data) {
+  //              console.log("error ");
+  //         } else {
+  //          setuserdata(data)
+  //              console.log("get data");
+  //          }
+  //    };
+  // console.log("datauser=>",getuserdata )
+  //    useEffect(() => {
+  //      getdatauser();
+  //    }, [])
+
+
+     const navigate = useNavigate();
+     const formik = useFormik({
+       initialValues : {
+         firstName : apiData?.firstName || '',
+         lastName: apiData?.lastName || '',
+         username : apiData?.username || '',
+        
+   
+       },
+       enableReinitialize: true,
+       validate : profileValidation,
+       validateOnBlur: false,
+       validateOnChange: false,
+       onSubmit : async values => {
+       values = await Object.assign(values, { profile : file || apiData?.profile || ''})
+       let updatePromise = updateUser(values);
+     
+      
+     
+       }
+     })
+     if(isLoading) return <h1 className='text-2xl font-bold'>isLoading</h1>;
+     if(serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
   return (
+    <>
+     
     <div className='displ flex'>
     <div>
    <SidebarRecruteur/>
@@ -10,177 +134,86 @@ function Recruter() {
     <div className='layout'>
     <Header/>
     <div className='cardMesoffre'>
+    
     <div className='row mx-2'>
-    <div className='cardrecut mt-10 col-6'>
-      <div className='mt-2 mb-2 '>nom d'offre</div>
+   
+
+
+  
+    <>
+    {
+    getoffredata.map((offre, key) => {
+            return (
+    <div className='cardrecut mt-10 col-5'>
+      <div className='mt-2 mb-2  mt-2 ' >{offre.Offrename}</div>
       <div className='row border'>
-        <div className='col-2'>
-          img
+        <div className='col-2  mt-2'>
+        <img src={ offre.profile|| file } alt="avatar" className='imgprofile'/>
         </div>
-        <div className="col-3">
-          nom
+        <div className="col-3  mt-2">
+        <div className='mx-3 col-5'>{offre.username}</div>
         </div>
-        <div className='col-2'>
-          score
+        <div className='col-2  mt-2'>score
         </div>
         <div className='col-4'>
           <div className='row'>
-          <div className='col-5 mt-1'>
+          <div className='col-5 mt-2 '>
             <button className='btn accepter'>accepte</button>   
             </div>
-            <div className='col-5'>
+            <div className='col-5 mx-2'>
             <button className='btn btnsupprimer'>refuse</button> 
             </div>
 
-          </div>
+    </div>
+    </div>
 
-        </div>
 
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          img
-        </div>
-        <div className="col-3">
-          nom
-        </div>
-        <div className='col-2'>
-          score
-        </div>
-        <div className='col-4'>
-          <div className='row'>
-          <div className='col-5 mt-1'>
-            <button className='btn accepter'>accepte</button>   
-            </div>
-            <div className='col-5'>
-            <button className='btn btnsupprimer'>refuse</button> 
-            </div>
 
-          </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>)})}
+</>
 
-        </div>
 
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          img
-        </div>
-        <div className="col-3">
-          nom
-        </div>
-        <div className='col-2'>
-          score
-        </div>
-        <div className='col-4'>
-          <div className='row'>
-          <div className='col-5 mt-1'>
-            <button className='btn accepter'>accepte</button>   
-            </div>
-            <div className='col-5'>
-            <button className='btn btnsupprimer'>refuse</button> 
-            </div>
 
-          </div>
 
-        </div>
 
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          img
-        </div>
-        <div className="col-3">
-          nom
-        </div>
-        <div className='col-2'>
-          score
-        </div>
-        <div className='col-4'>
-          <div className='row'>
-          <div className='col-5 mt-1'>
-            <button className='btn accepter'>accepte</button>   
-            </div>
-            <div className='col-5'>
-            <button className='btn btnsupprimer'>refuse</button> 
-            </div>
 
-          </div>
 
-        </div>
 
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          img
-        </div>
-        <div className="col-3">
-          nom
-        </div>
-        <div className='col-2'>
-          score
-        </div>
-        <div className='col-4'>
-          <div className='row'>
-          <div className='col-5 mt-1'>
-            <button className='btn accepter'>accepte</button>   
-            </div>
-            <div className='col-5'>
-            <button className='btn btnsupprimer'>refuse</button> 
-            </div>
 
-          </div>
 
-        </div>
 
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          img
-        </div>
-        <div className="col-3">
-          nom
-        </div>
-        <div className='col-2'>
-          score
-        </div>
-        <div className='col-4'>
-          <div className='row'>
-            <div className='col-5 mt-1'>
-            <button className='btn accepter'>accepte</button>   
-            </div>
-            <div className='col-5'>
-            <button className='btnsupprimer'>refuse</button> 
-            </div>
 
-          </div>
+    </div>
 
-        </div>
 
-      </div>
-      </div>
-      <div className='cardrecut mt-10 col-5 '>
-      </div>
-      </div>
-      <div className='row mx-2'>
-    <div className='cardrecut mt-10 col-6'>
-      </div>
-      <div className='cardrecut mt-10 col-5 '>
-      </div>
-      </div>
-      <div className='row mx-2'>
-    <div className='cardrecut mt-10 col-6'>
-      </div>
-      <div className='cardrecut mt-10 col-5 '>
-      </div>
-      </div>
-      </div>
+
+
+
+    </div>
+    </div>
+    
+    
+    </div>
+
+      
+
+     
       
       
       
       
-      
-      </div>
-      </div>
+   </>   
+     
   )
 }
 
